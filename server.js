0912661963bbfc486910dd9a686b0b2ab7a5dfbf
celikha <Hasan.Celik@informatik.uni-oldenburg.de>
2016-05-25 23:00:07 +0200

    added funktions and scheduler

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-var jsdom = require("jsdom");
+var jsdom = require("jsdom"),
+    childProcess = require('child_process'),
+    fs = require('fs'),
+    schedule = require("node-schedule"),
+    progress = require('request-progress'),
+    request = require('request');
 
-jsdom.env("https://www.reddit.com/r/" + (process.argv[2] || "EarthPorn"), ["http://code.jquery.com/jquery.js"],
-    function(error, window) {
-        var links = window.$("a.title").filter((i, value) => {
-            return /(\.jpg|\.jpeg|\.png)$/.test(value.href);
+schedule.scheduleJob('0 */2 * * *', function() {
+    getImageUrl(function(link) {
+        download(link, function(file) {
+            setWallpaper(file);
         });
-        console.log(links[Math.floor(links.length * Math.random())].href);
     });
+});
+
+
+function setWallpaper(image) {
+    childProcess.execFile('feh', ['--bg-scale', image], function(error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
+}
+
+function download(url, callback) {
+    progress(request(url))
+        .on('progress', function(state) {
+            console.log(state);
+        })
+        .pipe(fs.createWriteStream('tmp.jpg'))
+        .on('finish', function() {
+            callback('tmp.jpg');
+        });
+}
+
+function getImageUrl(callback) {
+    jsdom.env("https://www.reddit.com/r/" + (process.argv[2] || "EarthPorn"), ["http://code.jquery.com/jquery.js"], function(error, window) {
+        var links = window.$("a.title[href*='.jpg']");
+        var link = links[Math.floor(links.length * Math.random())].href;
+        if (link != 'undefined')
+            callback(link);
+    });
+}
