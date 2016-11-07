c7f3acb5801f4c139d819428b7d07322c3a258a1
celikha <Hasan.Celik@informatik.uni-oldenburg.de>
2016-11-07 16:32:18 +0100

    Bilder herunterladen statt direkt anzeigen

--- sh/wallpaper.sh
+++ sh/wallpaper.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-feh --bg-scale $(lynx -dump -listonly https://www.reddit.com/r/Wallpapers | grep -oP "http.+(jpg|png|jpeg)$" | sort -R | head -n 1)
+wget -q -O ~/.bg $(lynx -dump -listonly "https://www.reddit.com/r/wallpapers/hot" | grep -oP "http.+(jpg|png|jpeg)$" | sort -R | head -n 1)
+convert ~/.bg -blur 0x8 ~/.bg
+feh --bg-scale ~/.bg
